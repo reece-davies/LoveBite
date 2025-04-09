191211c503ad86e5c0884f7bcb3f01d37d791676
Reece Davies <153386893+reece-davies@users.noreply.github.com>
2025-04-09 18:46:11 +0000

    Handles URL query and GETs Firestore DB recipes that match recipe IDs

--- src/app/list/page.js
+++ src/app/list/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { db } from "@/lib/firebase.js"
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where, documentId } from "firebase/firestore";
 import { useSearchParams } from "next/navigation";
 
 export default function List() {
@@ -10,35 +10,58 @@ export default function List() {
 
   // Fetch Parameters data
   const searchParams = useSearchParams();
-  const dummyData = searchParams.get("dummyData");
+  //const dummyData = searchParams.get("dummyData");
+
+  const currentDate = new Date().toLocaleDateString("en-GB");
+  console.log("Current Date = ", currentDate);
 
   useEffect(() => {
-    const fetchRecipes = async () => {
+
+    const idsParam = searchParams.get("ids");
+
+    if(!idsParam) return;
+
+    let selectedIds = [];
+    try {
+      selectedIds = JSON.parse(idsParam);
+    } catch (error) {
+      console.error("Failed to parse selected IDs: ", error);
+      return;
+    }
+
+    console.log("SelectedId length = ", selectedIds.length);
+    if (!selectedIds.length) return;
+
+    const fetchSelectedRecipes = async () => {
       try {
-        // Process firestore data
-        const querySnapshot = await getDocs(collection(db, "recipes"));
+        // GET recipes that match ID from URL query
+        const recipesRef = collection(db, "recipes");
+        const q = query(recipesRef, where(documentId(), "in", selectedIds));
+        const querySnapshot = await getDocs(q);
+
         const recipeList = querySnapshot.docs.map(doc => ({
           id: doc.id,
           ...doc.data()
         }))
+
+        console.log("recipeList = ", recipeList)
         setRecipes(recipeList);
       } catch (error) {
-        console.log("ERROR = ", error)
+        console.log("Error fetching recipes: ", error)
       }
     };
 
-    fetchRecipes();
-  }, []);
+    fetchSelectedRecipes();
+  }, [searchParams]);
 
   return (
     // Removed styling: min-h-screen pb-18
     // border-4 border-red-500
     <div className="bg-zinc-100 min-h-screen grid grid-rows-[auto_1fr_20px] items-start justify-items-center p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="bg-white flex flex-col p-8 gap-8 row-start-2 items-center sm:items-start min-w-[300px] max-w-[500px]">
-        <h1 className="text-xl">Shopping List</h1> 
+        <h1 className="text-xl">Shopping List - {currentDate}</h1> 
 
         {/* Display Params data */}
-        <p>{dummyData}</p>
         
         {/* Recipe */}
         {recipes.map((recipe) => (
