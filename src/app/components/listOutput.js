b0a41b70f24c71cfb23d6e20ad1a1a030f9f8aca
Reece Davies <153386893+reece-davies@users.noreply.github.com>
2025-04-27 16:58:36 +0000

    Aligned ListOutput bullet points

--- src/app/components/listOutput.js
+++ src/app/components/listOutput.js
@@ -48,7 +48,7 @@ export default function ListOutput() {
 
   return (
     <div className="bg-zinc-100 min-h-screen grid grid-rows-[auto_1fr_20px] items-start justify-items-center p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="bg-white flex flex-col p-8 gap-8 row-start-2 items-center sm:items-start min-w-[300px] max-w-[500px]">
+      <main className="bg-white flex flex-col p-8 gap-4 row-start-2 items-stretch sm:items-start w-full min-w-[300px] max-w-[500px]">
         <h1 className="text-xl">Shopping List - {currentDate}</h1>
 
         {recipes.map((recipe) => (
